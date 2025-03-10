72501424fdc40c4fced7c62bb774f24ebc26cb64
Murcidum <73168797+Murcidum@users.noreply.github.com>
2025-03-10 09:34:30 +0800

    Test commit
    Test commit with API from SonixAPI

---
+++ SonixServer/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import com.example.musicservice.model.Album;
+import com.example.musicservice.model.Song;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Test API
+        Album album = new Album();
+        album.getId();
+    }
+}
